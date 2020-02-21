81a507588029968619122740eb0c4befd68e54ec
Scott Bowen <scottbowen@EKN-OSX100.local>
2020-02-21 08:55:11 -0500

    added very basic Firebase read/write functionality

--- pops/ViewController.swift
+++ pops/ViewController.swift
@@ -7,16 +7,77 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    var textOut : String!
+    
+    var ref : DatabaseReference!
+    
+    var databaseRefer : DatabaseReference!
+    var databaseHandle : DatabaseHandle!
+    
+    @IBOutlet weak var textOutput: UITextView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textOutput.text = ""
     }
 
     
     @IBAction func submit(_ sender: Any) {
+        print("Submit button tapped")
+        
+        let formatter = DateFormatter()
+        // initially set the format based on your datepicker date / server String
+        formatter.dateFormat = "MM-dd-yyyy HH:mm:ss"
+
+        let myString = formatter.string(from: Date()) // string purpose I add here
+        // convert your string to date
+        let yourDate = formatter.date(from: myString)
+        //then again set the date format whhich type of output you need
+        formatter.dateFormat = "MM-dd-yyyy HH:mm:ss"
+        // again convert your date to string
+        let myStringafd = formatter.string(from: yourDate!)
+
+        print(myStringafd)
+        
+        
+        
+        databaseRefer = Database.database().reference()
+//        databaseRefer.child("name").childByAutoId().setValue("Visal")
+//        databaseRefer.child("name").childByAutoId().setValue("Phanith")
+//        databaseRefer.child("name").childByAutoId().setValue(myStringafd)
+        databaseRefer.child("Date").child(myStringafd).setValue(myStringafd)
+        
+        
+         
+        databaseHandle = databaseRefer.child("Date").observe(.childAdded, with: { (data) in
+            let name: String = (data.value as? String)!
+            self.textOutput.text = name
+            debugPrint(name)
+        })
+        
+        
+//         let ref = Database.database().reference().child("users/scott")
+//
+//
+//         ref.observeSingleEvent(of: .value, with: { (snap : DataSnapshot)  in
+//
+//             print("\(String(describing:  snap.value))")
+//
+//         }) { (err: Error) in
+//
+//
+//             print("\(err.localizedDescription)")
+//
+//
+//
+//        }
+            
         
     }
     
